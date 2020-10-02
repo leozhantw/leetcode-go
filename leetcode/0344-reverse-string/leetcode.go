e2bca019b9c771e0c501aa30e82b9a2c0eb237f7
LeoZhan <leo3838ok@gmail.com>
2020-10-02 20:18:55 +0800

    refactor: optimize algorithm of 0344

--- leetcode/0344-reverse-string/leetcode.go
+++ leetcode/0344-reverse-string/leetcode.go
@@ -3,9 +3,7 @@ package leetcode
 func reverseString(s []byte) []byte {
 	n := len(s)
 	for i := 0; i < n/2; i++ {
-		temp := s[i]
-		s[i] = s[n-1-i]
-		s[n-1-i] = temp
+		s[i], s[n-1-i] = s[n-1-i], s[i]
 	}
 	return s
 }
