cf980e6666945cb42c2f7483871aed464dbebf28
LeoZhan <leo3838ok@gmail.com>
2020-10-02 19:13:20 +0800

    refactor: optimize the algorithm of 1395

--- leetcode/1395-count-number-of-teams/leetcode.go
+++ leetcode/1395-count-number-of-teams/leetcode.go
@@ -3,14 +3,23 @@ package leetcode
 func numTeams(rating []int) int {
 	var teams int
 	n := len(rating)
-	for i := 0; i < n-2; i++ {
-		for j := i + 1; j < n-1; j++ {
-			for k := j + 1; k < n; k++ {
-				if (rating[i] < rating[j] && rating[j] < rating[k]) || (rating[i] > rating[j] && rating[j] > rating[k]) {
-					teams++
-				}
+	for i := 1; i < n-1; i++ {
+		var leftSmall, leftLarge, rightSmall, rightLarge int
+		for j := i - 1; j >= 0; j-- {
+			if rating[j] < rating[i] {
+				leftSmall++
+			} else if rating[j] > rating[i] {
+				leftLarge++
 			}
 		}
+		for j := i + 1; j < n; j++ {
+			if rating[j] < rating[i] {
+				rightSmall++
+			} else if rating[j] > rating[i] {
+				rightLarge++
+			}
+		}
+		teams += (leftSmall * rightLarge) + (leftLarge * rightSmall)
 	}
 	return teams
 }
