11b372eedf20d06319260befe2c0a1a376494cb6
LeoZhan <leo3838ok@gmail.com>
2020-10-03 14:17:58 +0800

    feat: implement 0104-maximum-depth-of-binary-tree

---
+++ leetcode/0104-maximum-depth-of-binary-tree/leetcode_test.go
@@ -0,0 +1,49 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type give struct {
+	root *TreeNode
+}
+
+func Test_maxDepth(t *testing.T) {
+	tests := []struct {
+		give give
+		want int
+	}{
+		{
+			give: give{
+				root: &TreeNode{},
+			},
+			want: 1,
+		},
+		{
+			give: give{
+				root: &TreeNode{
+					Val: 3,
+					Left: &TreeNode{
+						Val: 9,
+					},
+					Right: &TreeNode{
+						Val: 20,
+						Left: &TreeNode{
+							Val: 15,
+						},
+						Right: &TreeNode{
+							Val: 7,
+						},
+					},
+				},
+			},
+			want: 3,
+		},
+	}
+
+	for _, tt := range tests {
+		got := maxDepth(tt.give.root)
+		assert.Equal(t, tt.want, got)
+	}
+}
